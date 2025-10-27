3700c395c2dcb2e1d3963f3e81161cf56439be75
Gerox <mezageronimo146@gmail.com>
2025-10-26 22:33:05 -0500

    agregamos a la ruta de las imagenes docs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pagina-colegio/docs/',
+  base: '/pagina-colegio/',
   plugins: [react(), tailwindcss()],
 })
